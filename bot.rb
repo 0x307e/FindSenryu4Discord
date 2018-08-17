54d36e7dd520d9a18e9c8d8565274a3646713e1f
Makoto Ito <3mdevmail@gmail.com>
2018-08-17 17:25:00 +0900

    :tada: add Bot
    とりあえず完成、Docker使わなければ動く

---
+++ bot.rb
@@ -0,0 +1,30 @@
+require 'yaml'
+require 'ikku'
+require 'discordrb'
+
+config = YAML.load_file("config.yml")
+bot = Discordrb::Commands::CommandBot.new token: config['token'], client_id: config['client_id'], prefix: config['prefix']
+
+bot.ready do
+  bot.game = "俳句検出"
+end
+
+bot.message do |event|
+author_id = event.author.id
+  if author_id == !config['bot_id']
+    return
+  else
+    haikureviewer = Ikku::Reviewer.new
+    haiku = haikureviewer.find(event.content)
+    if haiku then
+      event.send_message("<@#{author_id}> 俳句を検出しました！\n「#{haiku.phrases[0].join("")} #{haiku.phrases[1].join("")} #{haiku.phrases[2].join("")}」")
+    end
+    tankareviewer = Ikku::Reviewer.new(rule: [5, 7, 5, 7, 7])
+    tanka = tankareviewer.find(event.content)
+    if tanka then
+      event.send_message("<@#{author_id}> 短歌を検出しました！\n「#{tanka.phrases[0].join("")} #{tanka.phrases[1].join("")} #{tanka.phrases[2].join("")} #{tanka.phrases[3].join("")} #{tanka.phrases[4].join("")}」")
+    end
+  end
+end
+
+bot.run
