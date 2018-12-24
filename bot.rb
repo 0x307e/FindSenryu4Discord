79718363c66ad45e7d602e9f5ac55e3403483793
Makoto Ito <3mdevmail@gmail.com>
2018-12-25 00:24:33 +0900

    :+1: とりあえず実装完了
    あとでリファクタしないとまずい

--- bot.rb
+++ bot.rb
@@ -1,9 +1,60 @@
 require 'yaml'
 require 'ikku'
+require 'json'
+require 'mongo'
 require 'discordrb'
 
 config = YAML.load_file('config.yml')
-bot = Discordrb::Commands::CommandBot.new token: config['token'], client_id: config['client_id'], prefix: config['prefix']
+db = Mongo::Client.new(["#{config['mongo']['db_host']}:#{config['mongo']['db_port']}"], database: config['mongo']['db'])
+bot = Discordrb::Commands::CommandBot.new token: config['discord']['token'], client_id: config['discord']['client_id'], prefix: config['discord']['prefix']
+@collection = db[:ikku]
+
+
+def tanka(event)
+  tankareviewer = Ikku::Reviewer.new(rule: [5, 7, 5, 7, 7])
+  tanka = tankareviewer.find(event.content)
+  if tanka
+    data = {
+      sentence: [
+        tanka.phrases[0].join(""),
+        tanka.phrases[1].join(""),
+        tanka.phrases[2].join(""),
+        tanka.phrases[3].join(""),
+        tanka.phrases[4].join("")
+      ],
+      server: {
+        id: event.server.id,
+        name: event.server.name
+      }
+    }
+    @collection.insert_one(data)
+    data
+  else
+    return
+  end
+end
+
+def senryu(event)
+  senryureviewer = Ikku::Reviewer.new
+  senryu = senryureviewer.find(event.content)
+  if senryu
+    data = {
+      sentence: [
+        senryu.phrases[0].join(""),
+        senryu.phrases[1].join(""),
+        senryu.phrases[2].join("")
+      ],
+      server: {
+        id: event.server.id,
+        name: event.server.name
+      }
+    }
+    @collection.insert_one(data)
+    data
+  else
+    return
+  end
+end
 
 bot.ready do
   bot.game = '川柳&短歌検出'
@@ -13,15 +64,23 @@ bot.message do |event|
   author_id = event.author.id
   if author_id == !config['client_id']
     return
+  elsif event.content == "詠め"
+    ikkus = []
+    @collection.find('server.id' => event.server.id).each { |row|
+      ikkus.push(row['sentence'].join(' '))
+    }
+    event.send_message("ここで一句\n「#{ikkus.sample}」")
   elsif event.content == "詠むな"
     event.send_message("<@#{author_id}> 詠んでるのはお前やぞ")
   else
-    senryureviewer = Ikku::Reviewer.new
-    senryu = senryureviewer.find(event.content)
-    tankareviewer = Ikku::Reviewer.new(rule: [5, 7, 5, 7, 7])
-    tanka = tankareviewer.find(event.content)
-    event.send_message("<@#{author_id}> 川柳を検出しました！\n「#{senryu.phrases[0].join("")} #{senryu.phrases[1].join("")} #{senryu.phrases[2].join("")}」") if senryu
-    event.send_message("<@#{author_id}> 短歌を検出しました！\n「#{tanka.phrases[0].join("")} #{tanka.phrases[1].join("")} #{tanka.phrases[2].join("")} #{tanka.phrases[3].join("")} #{tanka.phrases[4].join("")}」") if tanka
+    senryu = senryu(event)
+    if senryu
+      event.channel.send_message("<@#{author_id}> 川柳を検出しました！\n「#{senryu[:sentence].join(' ')}」") if senryu
+      tanka = tanka(event)
+      if tanka
+        event.channel.send_message("<@#{author_id}> 短歌を検出しました！\n「#{tanka[:sentence].join(' ')}」") if tanka
+      end
+    end
   end
 end
 
