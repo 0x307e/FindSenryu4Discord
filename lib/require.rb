de63e8b219a21fd69fc74d73c24b4d31eca3512d
Makoto Ito <3mdevmail@gmail.com>
2019-03-02 11:57:51 +0900

    :+1: requireにRedisを追加, dbをmongoに変更

--- lib/require.rb
+++ lib/require.rb
@@ -2,11 +2,13 @@ require 'yaml'
 require 'ikku'
 require 'json'
 require 'mongo'
+require 'redis'
 require 'discordrb'
 require_relative './ikku.rb'
 
 config = YAML.load_file(File.expand_path('../config.yml', __dir__))
-db = Mongo::Client.new(["#{config['mongo']['db_host']}:#{config['mongo']['db_port']}"], database: config['mongo']['db'])
+mongo = Mongo::Client.new(["#{config['mongo']['db_host']}:#{config['mongo']['db_port']}"], database: config['mongo']['db'])
+@redis = Redis.new(host: config['redis']['db_host'], port: config['redis']['db_port'])
 @bot = Discordrb::Commands::CommandBot.new token: config['discord']['token'], client_id: config['discord']['client_id'], prefix: config['discord']['prefix']
-@collection = db[:ikku]
+@collection = mongo[:ikku]
 @config = config
