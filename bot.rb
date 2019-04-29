fc9b7678b39eda07b109763f066e041ab9f27034
makotia <3mdevmail@gmail.com>
2019-04-29 15:02:01 +0900

    :+1: 川柳めんどい

--- bot.rb
+++ bot.rb
@@ -1,4 +1,3 @@
-require 'pp'
 require_relative './lib/require.rb'
 
 bot = @bot
@@ -14,14 +13,14 @@ bot.message do |event|
     return
   elsif event.server == nil
     event.send_message('個チャはダメです')
-  elsif event.content == "詠め"
+  elsif event.content == '詠め'
     ikkus = []
     @collection.find('server.id' => event.server.id).each { |row|
       ikkus.push(row['sentence'])
     }
     event.send_message("ここで一句\n「#{ikkus.shuffle.shuffle.shuffle.sample[0]} #{ikkus.shuffle.shuffle.shuffle.sample[1]} #{ikkus.shuffle.shuffle.shuffle.sample[2]}」")
     @redis.set event.server.id, config['discord']['client_id']
-  elsif event.content == "詠むな"
+  elsif event.content == '詠むな'
     last_poet = @redis.get event.server.id
     if last_poet == nil
       event.send_message('まだ誰も詠んでないぞ')
@@ -36,11 +35,7 @@ bot.message do |event|
     senryu = senryu(event)
     if senryu
       event.channel.send_message("<@#{author_id}> 川柳を検出しました！\n「#{senryu[:sentence].join(' ')}」") if senryu
-      tanka = tanka(event)
       @redis.set event.server.id, author_id
-      if tanka
-        event.channel.send_message("<@#{author_id}> 短歌を検出しました！\n「#{tanka[:sentence].join(' ')}」") if tanka
-      end
     end
   end
 end
--- lib/ikku.rb
+++ lib/ikku.rb
@@ -17,30 +17,6 @@ def senryu(event)
       }
     }
     @collection.insert_one(data) if @collection.find('sentence' => data[:sentence])
-    data
-  else
-    return
-  end
-end
-
-def tanka(event)
-  tankareviewer = Ikku::Reviewer.new(rule: [5, 7, 5, 7, 7])
-  tanka = tankareviewer.find(event.content)
-  if tanka
-    data = {
-      type: 'tanka',
-      sentence: [
-        tanka.phrases[0].join(""),
-        tanka.phrases[1].join(""),
-        tanka.phrases[2].join(""),
-        tanka.phrases[3].join(""),
-        tanka.phrases[4].join("")
-      ],
-      server: {
-        id: event.server.id,
-        name: event.server.name
-      }
-    }
     @collection.insert_one(data) if @collection.find('sentence' => data[:sentence])
     data
   else
